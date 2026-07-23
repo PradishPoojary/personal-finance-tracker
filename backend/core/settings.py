bff11d9db0042d32b2d716f7949fd213d1a0fafa
PradishPoojary <pradishpoojary_444@outlool.com>
2026-07-23 22:11:24 +0530

    Secure CORS for production

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -156,4 +156,7 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://personal-finance-tracker-mu-eight.vercel.app", 
+]
